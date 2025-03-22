1c58ed301cdba58c37dc57299c53ac9022bfcd42
umairijaz62 <umairijaz2772@gmail.com>
2025-03-22 21:03:26 +0500

    solve issue

--- src/components/Home/Feature.tsx
+++ src/components/Home/Feature.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { blogs } from "@/constants/blogs/allblogs";
-import AnimatedButton from "@/custom/AnimateButton";
 import { motion } from "framer-motion";
 import { ArrowRight, Badge, Clock } from "lucide-react";
 import Link from "next/link";
+import { Button } from "../ui/button";
 import {
   Card,
   CardContent,
@@ -99,14 +99,12 @@ export default function Feature() {
             viewport={{ once: true }}
           >
             <Link href="/categories">
-              <AnimatedButton
-                text="View All Articles"
-                className="mt-8 p-2 bg-amber-500 hover:bg-amber-600 rounded-md text-white group flex items-center  transition-all duration-300 overflow-hidden"
-              >
+              <Button className="mt-8 p-2 bg-amber-500 hover:bg-amber-600 rounded-md text-white group flex items-center  transition-all duration-300 overflow-hidden">
+                View All Article
                 <span className="inline-block max-w-0 opacity-0 ml-0 group-hover:max-w-[16px] group-hover:ml-2 group-hover:opacity-100 transition-all duration-300">
                   <ArrowRight className="h-4 w-4" />
                 </span>
-              </AnimatedButton>
+              </Button>
             </Link>
           </motion.div>
         </div>
