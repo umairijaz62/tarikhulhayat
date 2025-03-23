2dce5bbc0df672c9c48ee7abe722750537dc6477
umairijaz62 <umairijaz2772@gmail.com>
2025-03-24 02:17:43 +0500

    add slug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+export function generateSlug(title: string): string {
+  return title.toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '');
+}
+
+export function cn(...classes: string[]): string {
+  return classes.filter(Boolean).join(' ');
+}
+
+
 export const connectDB = async () => {
   try {
     if (mongoose.connections[0].readyState) return;
