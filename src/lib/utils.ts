1c688885f92e0955f2688fd8243004d28ce4f69e
umairijaz62 <umairijaz2772@gmail.com>
2025-03-26 22:47:50 +0500

    Front-end again

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 
 export function generateSlug(title: string): string {
   return title.toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '');
@@ -8,15 +7,3 @@ export function cn(...classes: string[]): string {
   return classes.filter(Boolean).join(' ');
 }
 
-
-export const connectDB = async () => {
-  try {
-    if (mongoose.connections[0].readyState) return;
-    await mongoose.connect(process.env.MONGODB_URI as string, {
-      dbName: "Tarikh-ul-Hayat",
-    });
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (error) {
-    console.log("❌ MongoDB Connection Failed:", error);
-  }
-};
